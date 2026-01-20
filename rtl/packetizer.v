862ebbaba941ad0e3a6324fa82a56309e79dfe61
ShivamKurekar <kurekarshivam@gmail.com>
2026-01-20 15:40:34 +0530

    feat(packetizer.v): Paramaterised logic
    Updated the logic to paramterise the packetizer according to the DATA_WIDTH
    
    Signed-off-by: ShivamKurekar <kurekarshivam@gmail.com>

--- rtl/packetizer.v
+++ rtl/packetizer.v
@@ -1,32 +1,36 @@
-module packetizer (
-    input i_clk,
-    input i_valid,
-    input [7:0] i_data,
-    output [15:0] o_data,
-    output o_valid
+module packetizer #(parameter DATA_WIDTH = 16) (
+    input               i_clk,
+    input               i_valid,
+    input       [7:0]   i_data,
+
+    output reg  [DATA_WIDTH-1:0] o_data,
+    output reg          o_valid
 );
-    reg count = 0;
-    reg r_valid;
-    reg [15:0] r_data;
-    reg [7:0] temp_data;
+
+    // Assumptions:
+    // - DATA_WIDTH is a multiple of 8
+    // - Input is always 8-bit wide
+
+    localparam integer BYTES_COUNT = DATA_WIDTH / 8;
+    localparam integer COUNT_WIDTH = $clog2(BYTES_COUNT);
+
+    reg [DATA_WIDTH-1:0]  shift_reg;
+    reg [COUNT_WIDTH-1:0] byte_count;
 
     always @(posedge i_clk) begin
-        r_valid <= 0;
-        if(i_valid) begin
-            
-            if(!count) begin
-                temp_data <= i_data;
-                count <= count + 1;
-            end
-            else begin
-                r_data <= {temp_data, i_data};
-                count <= 0;
-                r_valid <= 1;
+        o_valid <= 1'b0;
+
+        if (i_valid) begin
+            shift_reg <= {shift_reg[DATA_WIDTH-9:0], i_data};
+
+            if (byte_count == BYTES_COUNT - 1) begin
+                o_data     <= {shift_reg[DATA_WIDTH-9:0], i_data};
+                byte_count <= {COUNT_WIDTH{1'b0}};
+                o_valid    <= 1'b1;
+            end else begin
+                byte_count <= byte_count + 1'b1;
             end
         end
     end
 
-    assign o_data = r_data;
-    assign o_valid = r_valid;
-
-endmodule //packetizer
+endmodule
